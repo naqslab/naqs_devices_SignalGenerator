aa9ffac38524027523566bf2934590e8383b2da8
Jason Pruitt <55477390+Json-To-String@users.noreply.github.com>
2025-06-17 09:53:44 -0400

    fix importlib import

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-import importlib.metadata as importlib_metadata
+import importlib.metadata
 
 # -- Project information -----------------------------------------------------
 
